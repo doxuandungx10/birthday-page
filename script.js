fca534e07c8d354c522bf21d1298c1fa80cd84b2
DO XUAN DUNG <doxuandungx10@gmail.com>
2021-09-13 22:34:40 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var a = prompt("Enter UserName");
+var b = prompt("Enter password");
+if (a == "May" && b == "m") {
+	window.location.href = "Start.html";
+}
+else {
+	alert("Wrong name or DateofBirth");
+	window.location.href = "index.html";
+}
