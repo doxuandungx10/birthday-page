3cd2a36be2cc2a03e6ccfe832a6bdc9e911f3cda
DO XUAN DUNG <doxuandungx10@gmail.com>
2021-09-17 20:27:09 +0700

    change

--- intro2/script.js
+++ intro2/script.js
@@ -4,13 +4,20 @@ $(function () {
     var head6 = $("#head6");
     var button1 = $("#button1");
     var button2 = $("#button2")
+    var btn_no = $("#btn_no");
+    var btn_yes2 = $("#btn_yes2")
+
     // head5.hide();
     button1.on({
         click: function () {
+            // btn_no.hide()
+            // btn_yes2.css("display", "inline-block")
+            // btn_yes2.css("color", "#fff")
+            // btn_yes2.css("background-color", "rgb(94, 28, 110)")
             document.getElementById("troll").play()
-            head4.fadeOut(2500)
+            head4.fadeOut(3000)
             button1.fadeOut(1500);
-            button2.fadeOut(200);
+            button2.fadeOut(1500);
             //     head4.html("Hehehe. ").delay(3000).fadeIn(3000)
             // }
             // );
@@ -34,10 +41,14 @@ $(function () {
     });
     button2.on({
         click: function () {
+            btn_no.hide()
+            btn_yes2.css("display", "inline-block")
+            btn_yes2.css("color", "#fff")
+            button2.css("background-color", "rgb(94, 28, 110)")
             document.getElementById("troll").play()
             head4.fadeOut(3000)
             button1.fadeOut(1500);
-            button2.fadeOut(200);
+            button2.fadeOut(1500);
             head5.delay(4000).animate(
                 {
                     opacity: "1"
